f90a3d580b3597bee2a81d4b5598210d7b35677d
suchan5 <suchan5@hotmail.com>
2026-06-30 12:37:13 +0800

    Run test for the first time

---
+++ src/main/java/com/suchan/qa/config/Config.java
@@ -0,0 +1,5 @@
+package com.suchan.qa.config;
+
+public class Config {
+    public static final String BASE_URL = "https://reqres.in";
+}
---
+++ src/test/java/com/suchan/qa/api/FirstApiTest.java
@@ -0,0 +1,11 @@
+package com.suchan.qa.api;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstApiTest {
+    @Test
+    void firstTest() {
+        System.out.println("Hello Junit!");
+
+    }
+}
