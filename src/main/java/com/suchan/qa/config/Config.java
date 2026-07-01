26860b52f60112af9d952861837738dd86ea875f
suchan5 <suchan5@hotmail.com>
2026-07-01 21:56:08 +0800

    Ignore config.properties

--- src/main/java/com/suchan/qa/config/Config.java
+++
@@ -1,5 +0,0 @@
-package com.suchan.qa.config;
-
-public class Config {
-    public static final String BASE_URL = "https://reqres.in";
-}
---
+++ src/test/java/com/suchan/qa/dto/CreateUserResponse.java
@@ -0,0 +1,4 @@
+package com.suchan.qa.dto;
+
+public class CreateUserResponse {
+}
