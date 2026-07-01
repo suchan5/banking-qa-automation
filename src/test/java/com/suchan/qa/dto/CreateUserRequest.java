b5a2bc023fcb9fa298efff76c92312904704650a
suchan5 <suchan5@hotmail.com>
2026-07-01 22:08:25 +0800

    Add CreateUserRequest DTO

--- src/test/java/com/suchan/qa/dto/CreateUserRequest.java
+++ src/test/java/com/suchan/qa/dto/CreateUserRequest.java
@@ -1,4 +1,29 @@
 package com.suchan.qa.dto;
 
 public class CreateUserRequest {
+    private String name;
+    private String job;
+
+    public CreateUserRequest() {
+    }
+
+    public CreateUserRequest(String name, String job) {
+        this.name = name;
+        this.job = job;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
 }
